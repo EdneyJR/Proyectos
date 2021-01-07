a52ae99f09a27d762a626f71c9c582128bd6f496
EdneyJR <77082927+EdneyJR@users.noreply.github.com>
2021-01-07 11:03:12 -0400

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,15 @@
+/*
+let pais = "Republica Dominicana";
+let continente = "Americano";
+let poblacion = 8000000;
+console.log(pais);
+console.log(continente);
+console.log(poblacion);
+
+let esIsla = true;
+let lenguaje;
+console.log(typeof esIsla);
+console.log(typeof poblacion);
+console.log(typeof pais);
+console.log(typeof lenguaje);
+*/
---
+++ script.js
@@ -0,0 +1,53 @@
+/*
+let js = 'asombroso';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Edney');
+console.log(24);
+
+let nombre = "JONNY";
+
+console.log(nombre);
+console.log(nombre);
+console.log(nombre);
+
+//Convencion de los nombres de variables
+let edney_stephanie = "ES";
+let $function = 27;
+
+let person = "Edney"
+let PI = 3.1415;
+
+let miPrimerTrabajo = "Telefonista"
+let miTrabajoActual = "AyR Agent"
+
+let trabajo1 = "telefonista"
+let trabajo2 = "ayr agent"
+
+console.log(miPrimerTrabajo);
+
+let javascriptEsDivertido = true;
+console.log(javascriptEsDivertido);
+
+//console.log(typeof true);
+console.log(typeof javascriptEsDivertido);
+//console.log(typeof 23);
+//console.log(typeof 'Edney');
+
+javascriptEsDivertido = 'SI!'
+console.log(typeof javascriptEsDivertido);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+let edad = 30;
+edad = 31;
+
+const anioDeNacimiento = 1991;
+anioDeNacimiento = 1990;
