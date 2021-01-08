69a9c0078b7f7ce2eae6cd2adc1d10b7f1eea4b3
EdneyJR <77082927+EdneyJR@users.noreply.github.com>
2021-01-07 21:09:43 -0400

    Update assignments.js

--- assignments.js
+++ assignments.js
@@ -1,15 +1,31 @@
 /*
-let pais = "Republica Dominicana";
-let continente = "Americano";
+//LECTURE: Values and Variables
+const pais = "Republica Dominicana";
+const continente = "Americano";
 let poblacion = 8000000;
 console.log(pais);
 console.log(continente);
 console.log(poblacion);
 
-let esIsla = true;
+//LECTURE: Data Types
+const esIsla = true;
 let lenguaje;
 console.log(typeof esIsla);
 console.log(typeof poblacion);
 console.log(typeof pais);
 console.log(typeof lenguaje);
+
+
+//LECTURE: let, const and var
+lenguaje = "Español";
+//continente = "Europeo";
 */
+
+//LECTURE: Basic Operators
+console.log(poblacion / 2);
+poblacion ++;
+console.log(poblacion);
+console.log(poblacion > 6000000);
+console.log(poblacion < 33000000);
+const descripcion = pais + " esta en el continente " + continente + " y sus " + poblacion + " de personas hablan " + lenguaje;
+console.log(descripcion);
