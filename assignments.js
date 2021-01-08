c21c4f49967939bf030cfebc2e062079f2235777
EdneyJR <77082927+EdneyJR@users.noreply.github.com>
2021-01-07 21:49:06 -0400

    Update assignments.js

--- assignments.js
+++ assignments.js
@@ -19,7 +19,6 @@ console.log(typeof lenguaje);
 //LECTURE: let, const and var
 lenguaje = "Español";
 //continente = "Europeo";
-*/
 
 //LECTURE: Basic Operators
 console.log(poblacion / 2);
@@ -29,3 +28,21 @@ console.log(poblacion > 6000000);
 console.log(poblacion < 33000000);
 const descripcion = pais + " esta en el continente " + continente + " y sus " + poblacion + " de personas hablan " + lenguaje;
 console.log(descripcion);
+
+
+//Coding Challenge #1
+// Data 1
+const markMass = 78, markHeight = 1.69, johnMass = 92, johnHeight = 1.95;
+const markBMI = markMass / markHeight ** 2, johnBMI = johnMass / johnHeight ** 2;
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI);
+console.log(markHigherBMI);
+
+//Data 2
+const markMass = 95, markHeight = 1.88, johnMass = 85, johnHeight = 1.76;
+const markBMI = markMass / (markHeight * markHeight), johnBMI = johnMass / (johnHeight * johnHeight);
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI);
+console.log(markHigherBMI);
+*/
+
